c49c6846ebbb6f7c7831f9f9841760643c9fa2f3
MatteV02 <veronimatteo1@gmail.com>
2024-08-16 12:31:16 +0200

    Slight changes in DBEntityManager

--- commonModule/src/main/kotlin/dbManagement/DBEntityManager.kt
+++ commonModule/src/main/kotlin/dbManagement/DBEntityManager.kt
@@ -4,6 +4,10 @@ import entities.*
 import org.hibernate.SessionFactory
 import org.hibernate.annotations.NamedQuery
 import org.hibernate.annotations.processing.CheckHQL
+import org.hibernate.cfg.AvailableSettings
+import org.hibernate.cfg.Configuration
+import org.hibernate.cfg.JdbcSettings.*
+import org.hibernate.tool.schema.Action
 
 @CheckHQL
 @NamedQuery(
@@ -27,50 +31,59 @@ import org.hibernate.annotations.processing.CheckHQL
     query = "from Place"
 )
 object DBEntityManager {
-    var sessionFactory: SessionFactory? = null
+    fun getDefault(): SessionFactory = Configuration()
+        .addAnnotatedClass(Patient::class.java)
+        .addAnnotatedClass(Place::class.java)
+        .addAnnotatedClass(Visit::class.java)
+        .addAnnotatedClass(DBEntityManager::class.java)
+        .setProperty(AvailableSettings.JAKARTA_JDBC_USER, "sa")
+        .setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "")
+        .setProperty(AvailableSettings.JAKARTA_HBM2DDL_CREATE_SCHEMAS, true)
+        .setProperty(AvailableSettings.JAKARTA_HBM2DDL_DATABASE_ACTION, Action.UPDATE)
+        .setProperty("hibernate.agroal.maxSize", 20)
+        .setProperty(SHOW_SQL, true)
+        .setProperty(FORMAT_SQL, true)
+        .setProperty(HIGHLIGHT_SQL, true)
+        .setProperty(AvailableSettings.JAKARTA_JDBC_URL,  "jdbc:h2:db")
+        .buildSessionFactory()
+
+    var sessionFactory: SessionFactory = getDefault()
 
     fun insert(ob: Any) {
-        sessionFactory?.inTransaction { session ->
+        sessionFactory.inTransaction { session ->
             session.persist(ob)
-        } ?: throw DBEntityManagerNotInitializedException()
+        }
     }
 
     fun update(ob: Any) {
-        sessionFactory?.inTransaction { session ->
+        sessionFactory.inTransaction { session ->
             session.merge(ob)
-        } ?: throw DBEntityManagerNotInitializedException()
+        }
     }
 
     fun remove(ob: Any) {
-        sessionFactory?.inTransaction { session ->
+        sessionFactory.inTransaction { session ->
             session.remove(ob)
-        } ?: throw DBEntityManagerNotInitializedException()
+        }
     }
 
     fun getPatients() : List<Patient> {
-        return DBEntityManager_.getPatientsFromDB(sessionFactory?.createEntityManager())
-            ?: throw DBEntityManagerNotInitializedException()
+        return DBEntityManager_.getPatientsFromDB(sessionFactory.createEntityManager())
     }
 
     fun getPlaces() : List<Place> {
-        return DBEntityManager_.getPlacesFromDB(sessionFactory?.createEntityManager())
-            ?: throw DBEntityManagerNotInitializedException()
+        return DBEntityManager_.getPlacesFromDB(sessionFactory.createEntityManager())
     }
 
     fun getVisits() : List<Visit> {
-        return DBEntityManager_.getVisitsFromDB(sessionFactory?.createEntityManager())
-            ?: throw DBEntityManagerNotInitializedException()
+        return DBEntityManager_.getVisitsFromDB(sessionFactory.createEntityManager())
     }
 
     fun getVisitsOfPatient(patient: Patient) : List<Visit> {
-        return DBEntityManager_.getVisitsRelatedToPatientFromDB(sessionFactory?.createEntityManager(), patient)
-            ?: throw DBEntityManagerNotInitializedException()
+        return DBEntityManager_.getVisitsRelatedToPatientFromDB(sessionFactory.createEntityManager(), patient)
     }
 
     fun getSpecialists() : List<String> {
-        return DBEntityManager_.getSpecialistsFromDB(sessionFactory?.createEntityManager())
-            ?: throw DBEntityManagerNotInitializedException()
+        return DBEntityManager_.getSpecialistsFromDB(sessionFactory.createEntityManager())
     }
-
-    class DBEntityManagerNotInitializedException : RuntimeException("DBEntityManager not initialized")
 }
